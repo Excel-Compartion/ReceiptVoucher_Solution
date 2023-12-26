e1f336e8b0fd59cc863e82b85357e33b5f1fc914
Waleed Bensumaidea <waleed.com145@gmail.com>
2023-12-26 08:01:27 +0300

    First Migration

---
+++ ReceiptVoucher.EF/Migrations/20231226045803_firstMigration.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReceiptVoucher.EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class firstMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Branches",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Branches", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CompanyInfo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    Mobile = table.Column<int>(type: "int", nullable: true),
+                    LicenseNumber = table.Column<int>(type: "int", nullable: true),
+                    Telephone = table.Column<int>(type: "int", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: true),
+                    Web = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: true),
+                    IVT = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CompanyInfo", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sub_Projects",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProjectId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    Duration = table.Column<int>(type: "int", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sub_Projects", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Sub_Projects_Projects_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "Projects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Receipts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ReceivedFrom = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    ReceivedBy = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: true),
+                    TotalAmount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    BranchId = table.Column<int>(type: "int", nullable: false),
+                    Sub_ProjectId = table.Column<int>(type: "int", nullable: false),
+                    ForPurpose = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    Date = table.Column<DateOnly>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Receipts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Receipts_Branches_BranchId",
+                        column: x => x.BranchId,
+                        principalTable: "Branches",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Receipts_Sub_Projects_Sub_ProjectId",
+                        column: x => x.Sub_ProjectId,
+                        principalTable: "Sub_Projects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Receipts_BranchId",
+                table: "Receipts",
+                column: "BranchId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Receipts_Sub_ProjectId",
+                table: "Receipts",
+                column: "Sub_ProjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sub_Projects_ProjectId",
+                table: "Sub_Projects",
+                column: "ProjectId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CompanyInfo");
+
+            migrationBuilder.DropTable(
+                name: "Receipts");
+
+            migrationBuilder.DropTable(
+                name: "Branches");
+
+            migrationBuilder.DropTable(
+                name: "Sub_Projects");
+
+            migrationBuilder.DropTable(
+                name: "Projects");
+        }
+    }
+}
---
+++ ReceiptVoucher.EF/ReceiptVoucherDbContext.cs
@@ -0,0 +1,28 @@
+﻿
+namespace ReceiptVoucher.EF;
+
+public class ReceiptVoucherDbContext : DbContext
+{
+    public ReceiptVoucherDbContext()
+    {
+    }
+
+    public ReceiptVoucherDbContext(DbContextOptions<ReceiptVoucherDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Branch> Branches { get; set; }
+
+    public virtual DbSet<CompanyInfo> CompanyInfos { get; set; }
+
+    public virtual DbSet<Project> Projects { get; set; }
+
+    public virtual DbSet<Receipt> Receipts { get; set; }
+
+    public virtual DbSet<SubProject> SubProjects { get; set; }
+
+
+
+
+}
