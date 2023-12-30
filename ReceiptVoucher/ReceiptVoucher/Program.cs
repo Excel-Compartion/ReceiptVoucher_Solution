012e7d404d40d50b2465a6781e3a323a9c03e392
Waleed Bensumaidea <waleed.com145@gmail.com>
2023-12-30 11:29:44 +0300

    Add MudBlazor Template on Server

--- ReceiptVoucher/ReceiptVoucher/Program.cs
+++ ReceiptVoucher/ReceiptVoucher/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using MudBlazor.Services;
 using ReceiptVoucher.Client.Pages;
 //using ReceiptVoucher.Components;
 using ReceiptVoucher.Core;
@@ -11,6 +12,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddMudServices();  // MudBlazor
 
 var connectionString = builder.Configuration.GetConnectionString("DefualtConnection")
             ?? throw new InvalidOperationException("No Connection String Was Found");
