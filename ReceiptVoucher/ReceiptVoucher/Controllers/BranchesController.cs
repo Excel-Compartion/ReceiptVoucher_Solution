fee271c21ae84fea1ccc3b1fa30c1159578d23b7
Waleed Bensumaidea <waleed.com145@gmail.com>
2024-01-02 14:33:54 +0300

    Complete Creating Branch

---
+++ ReceiptVoucher/ReceiptVoucher/Controllers/BranchesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ReceiptVoucher.Core;
+
+namespace ReceiptVoucher.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BranchesController : ControllerBase
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+        public BranchesController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet("GetAllAsync")]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            return Ok(await _unitOfWork.Branches.GetAllAsync());
+        }
+
+       [HttpPost("AddOneAsync")]
+        public async Task<IActionResult> AddOne(Branch branch)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            // let UnitOfWork  do creating and saving to database
+
+            await _unitOfWork.Branches.AddOneAsync(branch);
+
+            return Ok("Branch Created Succesfully.");
+        }
+
+
+        //[HttpDelete]
+    }
+}
