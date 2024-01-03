9380bc39389397a2cf3522491f446eb822f28218
Waleed Bensumaidea <waleed.com145@gmail.com>
2024-01-03 13:08:46 +0300

    Update Folder Name

---
+++ ReceiptVoucher.Core/Entities/Receipt.cs
@@ -0,0 +1,34 @@
+﻿namespace ReceiptVoucher.Core.Entities;
+
+public class Receipt
+{
+    [Key]
+    public int Id { get; set; }
+
+    [StringLength(250)]
+    public string ReceivedFrom { get; set; } = null!;
+
+    [StringLength(10)]
+    public string? ReceivedBy { get; set; }     // Foreign Key From User
+
+    [Column(TypeName = "decimal(18, 2)")]
+    public decimal TotalAmount { get; set; }
+
+    public int BranchId { get; set; }   // Foreign Key
+
+    [Column("Sub_ProjectId")]
+    public int SubProjectId { get; set; }   // Foreign Key
+
+    [StringLength(500)]
+    public string ForPurpose { get; set; } = null!;
+
+    public DateOnly Date { get; set; }
+
+
+    //-------- Navigation Properties ---
+    [ForeignKey("BranchId")]
+    public Branch Branch { get; set; } = null!;
+
+    [ForeignKey("SubProjectId")]
+    public SubProject SubProject { get; set; } = null!;
+}
---
+++ ReceiptVoucher.Core/Entities/SubProject.cs
@@ -0,0 +1,27 @@
+﻿namespace ReceiptVoucher.Core.Entities;
+
+[Table("Sub_Projects")]
+public class SubProject
+{
+    [Key]
+    public int Id { get; set; }
+
+    public int ProjectId { get; set; }  // Foreign Key
+
+    [StringLength(250)]
+    public string Name { get; set; } = null!;
+
+    public int? Duration { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime CreatedDate { get; set; }
+
+    public bool IsActive { get; set; }
+
+    //-------- Navigation Properties ---
+
+    [ForeignKey("ProjectId")]
+    public Project Project { get; set; } = null!;
+
+    public ICollection<Receipt> Receipts { get; set; } = new List<Receipt>();
+}
