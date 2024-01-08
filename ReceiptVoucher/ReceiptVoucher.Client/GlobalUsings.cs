87b56711dd28991dc78ad210bf5e8c3fbf782186
Waleed Bensumaidea <waleed.com145@gmail.com>
2024-01-08 13:33:16 +0300

    Move Branches page into Client

---
+++ ReceiptVoucher/ReceiptVoucher.Client/GlobalUsings.cs
@@ -0,0 +1,12 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using Microsoft.EntityFrameworkCore;
+global using ReceiptVoucher.Core.Entities;
+//global using ReceiptVoucher.Core.Modelsk;
+global using ReceiptVoucher.Client.Pages;
+global using ReceiptVoucher.EF;
+global using ReceiptVoucher.Core.Models.ViewModels;
+global using ReceiptVoucher.Core.Models.Dtos;
+global using MudBlazor;
+global using ReceiptVoucher.Core;
+
