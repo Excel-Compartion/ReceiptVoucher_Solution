43dad20d0b1843e146228c255953761bf0101da2
Awad_Mohammed <awadmohammed001mq@gmail.com>
2024-01-28 16:55:03 +0300

    LastChange

---
+++ ReceiptVoucher.Core/Entities/LoginModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReceiptVoucher.Core.Entities
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "يرجى اخال البريد الالكتروني ")]
+        [EmailAddress(ErrorMessage = "يرجى ادخال البريد الالكتروني بشكل صحيح")]
+        public string Email { get; set; } = null!;
+
+        [Required(ErrorMessage ="يرجى اخال كلمه السر")]
+        public string Password { get; set; }=null!;
+
+        public bool RemmberMe { get; set; }
+    }
+}
