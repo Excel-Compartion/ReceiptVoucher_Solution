e4c7a61d8965f17a05d952ab852a6e537baa6641
Waleed Bensumaidea <waleed.com145@gmail.com>
2024-02-01 18:51:57 +0300

    AddingJWT-- Add User Models

---
+++ ReceiptVoucher/ReceiptVoucher/Controllers/UsersController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ReceiptVoucher.Core.Consts;
+using ReceiptVoucher.Core.Identity;
+using ReceiptVoucher.Core.Models;
+
+namespace ReceiptVoucher.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    //[Authorize(Roles = RolesNames.Admin)]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public UsersController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        [HttpGet]
+        [Route("api/users")]
+        public async Task<IActionResult> GetUsersWithRoles()
+        {
+            var users = await _userManager.Users.ToListAsync();
+            var userViewModels = new List<UserViewModel>();
+
+            foreach (var user in users)
+            {
+                var userRoles = await _userManager.GetRolesAsync(user);
+                userViewModels.Add(new UserViewModel
+                {
+                    Id = user.Id,
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    Email = user.Email,
+                    UserName = user.UserName,
+                    Roles = userRoles
+                });
+            }
+
+            return Ok(userViewModels);
+        }
+
+
+
+    }
+}
