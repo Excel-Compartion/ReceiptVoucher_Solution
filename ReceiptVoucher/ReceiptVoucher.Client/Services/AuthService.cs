221b1c668fdf73fb759b04a0887c71fd33dc16fe
Waleed Bensumaidea <waleed.com145@gmail.com>
2024-02-07 02:47:42 +0300

    AddingJWT-- Fix When RedirectUser To home

--- ReceiptVoucher/ReceiptVoucher.Client/Services/AuthService.cs
+++ ReceiptVoucher/ReceiptVoucher.Client/Services/AuthService.cs
@@ -14,11 +14,13 @@ namespace ReceiptVoucher.Client.Services
         private readonly HttpClient _httpClient;
         private readonly AuthenticationStateProvider _authenticationStateProvider;
         private readonly NavigationManager _navigationManager;
-        public AuthService(HttpClient httpClient, NavigationManager navigationManager, AuthenticationStateProvider authenticationStateProvider)
+        private readonly ISnackbar _snackbar;
+        public AuthService(HttpClient httpClient, NavigationManager navigationManager, AuthenticationStateProvider authenticationStateProvider, ISnackbar snackbar)
         {
             _httpClient = httpClient;
             _navigationManager = navigationManager;
             _authenticationStateProvider = authenticationStateProvider;
+            _snackbar = snackbar;
         }
 
 
@@ -42,21 +44,23 @@ namespace ReceiptVoucher.Client.Services
 
         public async Task CheckIfNotAdminRedirectToLoginAsync()
         {
-            var authState = await _authenticationStateProvider.GetAuthenticationStateAsync();
-            var user = authState.User;
-            var role = user.FindFirst("roles")?.Value;
+            var authState = await _authenticationStateProvider?.GetAuthenticationStateAsync();
+            var user = authState?.User;
+            var role = user?.FindFirst("roles")?.Value;
 
-            if (role.Equals(RolesNames.User))
+            if (role?.Equals(RolesNames.User) == true)
             {
-                _navigationManager.NavigateTo("Receipt");
+                _navigationManager?.NavigateTo("Receipt");
+                _snackbar?.Add("لاتسمح لك صلاحيتك بالوصول الى هذه الصفحه", Severity.Error);
+
                 return;
             }
 
-            else if (!user.Identity.IsAuthenticated || !role.Equals(RolesNames.Admin))
+            else if (user?.Identity?.IsAuthenticated == false || role?.Equals(RolesNames.Admin) == false)
             {
-                _navigationManager.NavigateTo("login");
-                _navigationManager.Refresh();
-
+                _navigationManager?.NavigateTo("login");
+                _navigationManager?.Refresh();
+                _snackbar?.Add("انت لست مسجل الدخول اصلا لكي تصل الى هذه الصفحه,النظام محمي بقوه", Severity.Error);
             }
         }
     }
