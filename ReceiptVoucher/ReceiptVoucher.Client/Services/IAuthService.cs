95e8ffe8a0e065a84a10d5fe9bfc03f2c00befeb
Waleed Bensumaidea <waleed.com145@gmail.com>
2024-03-15 23:13:50 +0300

    Add  GetUserIdByFullNameAsync In ClientAuthService

--- ReceiptVoucher/ReceiptVoucher.Client/Services/IAuthService.cs
+++ ReceiptVoucher/ReceiptVoucher.Client/Services/IAuthService.cs
@@ -16,6 +16,15 @@ namespace ReceiptVoucher.Client.Services
         Task<UserViewModel?> GetCurrentUserDetailsFromApiAsync();
         Task<UserViewModel?> GetUserDetailsByIdAsync(string userId);
 
+        /// <summary>
+        /// <b>WALEED MOHAMMED</b> Edited At 2024-03-015
+        /// <br></br>
+        /// firstName And Last Name الخاص بالمستخدم بتمرير  ID هذه الداله تقوم بارجاع 
+        /// </summary>
+        /// <param name="firstName"></param>
+        /// <param name="lastName"></param>
+        /// <returns></returns>
+        Task<string?> GetUserIdByFullNameAsync(string firstName, string lastName);
 
         //Task<string> AddRoleAsync(AddRoleModel roleModel);
         //Task<AuthModel> RefreshTokenAsync(string token);
